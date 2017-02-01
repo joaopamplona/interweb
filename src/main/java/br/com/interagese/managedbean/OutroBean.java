97040800d85b9ec990403e476bef48e9eb34187c
josevjunior <josev.junior@outlook.com>
2017-02-01 15:35:20 -0300

    Arquivos Adicionados

---
+++ src/main/java/br/com/interagese/managedbean/OutroBean.java
@@ -0,0 +1,16 @@
+
+package br.com.interagese.managedbean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OutroBean {
+    
+    private String outroNome = "josé";
+
+    public String getOutroNome() {
+        return outroNome;
+    }
+    
+    
+}
