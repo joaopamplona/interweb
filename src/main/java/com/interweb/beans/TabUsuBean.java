da972a1ad3916160185699ae74e27c562eccd1d2
joaopamplona <joaovplayer@gmail.com>
2017-05-18 15:54:09 -0300

    Joao
    Mensagens de erro no loogin

--- src/main/java/com/interweb/beans/TabUsuBean.java
+++ src/main/java/com/interweb/beans/TabUsuBean.java
@@ -43,50 +43,65 @@ public class TabUsuBean {
             queryUsuario.setParameter("senha", tabusu.getSenha());
             
             //Pega a lista de usuarios
+            
             List<Tabusu> resultTabUsu = queryUsuario.getResultList();
+            
+            if(resultTabUsu.isEmpty()){
+                FacesContext.getCurrentInstance().addMessage("myform:frmlogin", new FacesMessage("Usuário ou senha incorretos"));
+            } else {
+                for (Tabusu usuario : resultTabUsu) {
+                    //Faz o select de sistemas e seta codsis vindo do select de usuarios
+                    Query queryTabsis = JPAUtil.getEntityManager().createNamedQuery("Tabsis.buscaSistemaDoUsuario");
+                    queryTabsis.setParameter("codsis", usuario.getSistema());
 
-            for (Tabusu usuario : resultTabUsu) {
-                //Faz o select de sistemas e seta codsis vindo do select de usuarios
-                Query queryTabsis = JPAUtil.getEntityManager().createNamedQuery("Tabsis.buscaSistemaDoUsuario");
-                queryTabsis.setParameter("codsis", usuario.getSistema());
+                    //Pega a lista de sistemas
+                    List<Tabsis> resultTabsis = queryTabsis.getResultList();
+                    
+                    if(resultTabsis.isEmpty()){
+                        FacesContext.getCurrentInstance().addMessage("myform:frmlogin", new FacesMessage("Tabsis nao encontrada"));
+                    } else {
 
-                //Pega a lista de sistemas
-                List<Tabsis> resultTabsis = queryTabsis.getResultList();
+                        for (Tabsis sistema : resultTabsis) {
+                            //Faz o select de menus e seta o parametro codsis vindo do select de sistemas
+                            Query queryMenuitem = JPAUtil.getEntityManager().createNamedQuery("Menuitem.buscaMenuDoSistema");
+                            queryMenuitem.setParameter("codsis", sistema.getCodsis());
 
-                for (Tabsis sistema : resultTabsis) {
-                    //Faz o select de menus e seta o parametro codsis vindo do select de sistemas
-                    Query queryMenuitem = JPAUtil.getEntityManager().createNamedQuery("Menuitem.buscaMenuDoSistema");
-                    queryMenuitem.setParameter("codsis", sistema.getCodsis());
-
-                    //Não precisa repetir
-                    List<Menuitem> resultMenuitem = queryMenuitem.getResultList();
-
-                    //Popula o menu com os itens da lista acima
-                    for (Menuitem menuitem : resultMenuitem) {
-                        String label = menuitem.getDescricao();
-                        DefaultSubMenu item = new DefaultSubMenu(label);
-                        if(label.equals("Cadastros")){
-                            DefaultMenuItem subitem = new DefaultMenuItem("Veículos");
-                            subitem.setUrl("cadastro_veiculo.xhtml?faces-redirect=true");
-                                    
-                            item.setId("SubMenuCadastros");
-                            item.addElement(subitem);
+                            //Não precisa repetir
+                            List<Menuitem> resultMenuitem = queryMenuitem.getResultList();
                             
-                        }
-                        
-                        
-                        menuBar.getMenubar().addElement(item);
-                        
-                    }
-                    
-                    
-                    FacesContext.getCurrentInstance().addMessage("myform:frmlogin", new FacesMessage("Usuário Logado!"));
-                    Utilitarios.RedirecionarPagina("views/pagina_principal.xhtml?faces-redirect=true");
-                    return null;
-                }
+                            if(resultMenuitem.isEmpty()){
+                                FacesContext.getCurrentInstance().addMessage("myform:frmlogin", new FacesMessage("Este usuário não tem acesso a nenhum sistema "));
+                            } else {
 
+                                //Popula o menu com os itens da lista acima
+                                for (Menuitem menuitem : resultMenuitem) {
+                                    String label = menuitem.getDescricao();
+                                    DefaultSubMenu item = new DefaultSubMenu(label);
+                                    if(label.equals("Cadastros")){
+                                        DefaultMenuItem subitem = new DefaultMenuItem("Veículos");
+                                        subitem.setUrl("cadastro_veiculo.xhtml?faces-redirect=true");
+
+                                        item.setId("SubMenuCadastros");
+                                        item.addElement(subitem);
+
+                                    }
+
+
+                                    menuBar.getMenubar().addElement(item);
+
+                                }
+                                
+                                FacesContext.getCurrentInstance().addMessage("myform:frmlogin", new FacesMessage("Usuário Logado!"));
+                                Utilitarios.RedirecionarPagina("views/pagina_principal.xhtml?faces-redirect=true");
+                                
+                            }
+
+                            return null;
+                        }
+                    }
+
+                }
             }
-  
         } catch (Exception e) {
             e.printStackTrace();
             FacesContext.getCurrentInstance().addMessage("myform:frmlogin", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", e.getLocalizedMessage()));
--- target/generated-sources/annotations/com/interweb/model/Tabusu_.java
+++ target/generated-sources/annotations/com/interweb/model/Tabusu_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-22T11:04:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-17T09:42:52")
 @StaticMetamodel(Tabusu.class)
 public class Tabusu_ { 
 
