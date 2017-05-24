d3505cbef73bdbce85e0038bf1359713e6e12b45
joaopamplona <joaovplayer@gmail.com>
2017-05-24 16:13:09 -0300

    joao
    veiculos

--- target/generated-sources/annotations/com/interweb/model/Tabsis_.java
+++ target/generated-sources/annotations/com/interweb/model/Tabsis_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-18T16:21:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-24T16:10:48")
 @StaticMetamodel(Tabsis.class)
 public class Tabsis_ { 
 
